d9fd3308d44136d3fc9d69e74a6d03f64bf9b273
NiveditJain <nivedit@aikin.club>
2025-07-11 14:52:40 +0530

    fix build issues

--- landing-page/src/components/magicui/animated-list.tsx
+++ landing-page/src/components/magicui/animated-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { AnimatePresence, motion } from "motion/react";
+import { AnimatePresence, motion, MotionProps } from "motion/react";
 import React, {
   ComponentPropsWithoutRef,
   useEffect,
@@ -10,11 +10,11 @@ import React, {
 } from "react";
 
 export function AnimatedListItem({ children }: { children: React.ReactNode }) {
-  const animations = {
+  const animations: MotionProps = {
     initial: { scale: 0, opacity: 0 },
     animate: { scale: 1, opacity: 1, originY: 0 },
     exit: { scale: 0, opacity: 0 },
-    transition: { type: "spring", stiffness: 350, damping: 40 },
+    transition: { type: "spring" as const, stiffness: 350, damping: 40 },
   };
 
   return (
