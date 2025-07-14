239dce1ac9d49d339559879efdd4c05fba3f5f50
Nivedit Jain <niveditjain@outlook.com>
2025-07-14 19:20:19 +0530

    Fixing email validation warning, added Redis to Kubernetes and moved logging to console for k8ns (#53)
    * added redis
    
    * fixes

--- api-server/app/singletons/logs_manager.py
+++ api-server/app/singletons/logs_manager.py
@@ -3,7 +3,6 @@ import pathlib
 import os
 import logging
 from typing import Optional
-from logging.handlers import RotatingFileHandler
 from .SingletonDecorator import singleton
 
 
@@ -13,23 +12,8 @@ class LogsManager:
     This class is used to manage the logs for the application
     """
 
-    def __init__(
-        self,
-        path: str = os.path.join(pathlib.Path(__file__).parent.parent.parent, "logs"),
-        file_name: Optional[str] = None,
-        max_bytes: int = 10 * 1024 * 1024,  # 10MB
-        backup_count: int = 5,
-    ):
-        # Create the logs directory if it doesn't exist
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-
-        # Set up rotating file handler
-        file_handler = RotatingFileHandler(
-            os.path.join(path, file_name if file_name else "logs.json5"),
-            maxBytes=max_bytes,
-            backupCount=backup_count,
-            encoding="utf-8",
-        )
+    def __init__(self):
+        handler = logging.StreamHandler()
 
         structlog.configure(
             processors=[
@@ -44,9 +28,9 @@ class LogsManager:
             processor=structlog.processors.JSONRenderer(),
         )
         
-        file_handler.setFormatter(formatter)
+        handler.setFormatter(formatter)
         logger = logging.getLogger()
-        logger.addHandler(file_handler)
+        logger.addHandler(handler)
         logger.setLevel(logging.INFO)
 
         self.logger = structlog.get_logger()
